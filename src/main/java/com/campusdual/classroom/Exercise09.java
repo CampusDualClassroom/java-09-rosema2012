20515c735c6a4600bfdf7f9c7a8423424bd06b7e
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-25 23:00:51 +0200

    Ejercicio 9 terminado

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(30);
     }
 
     //TODO ↓
@@ -12,6 +12,10 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int acu = 0;
+        do{
+            acu++;
+            System.out.println("Entra con valor: "+(acu-1)+", sale con valor: "+(acu));
+        }while(acu<num);
     }
 }
